35c2772d5e5508366b1e1a5d04e710845008e14f
Achal Talati <achal.talati@oracle.com>
2024-02-20 14:49:50 +0530

    removed test-project
    Signed-off-by: Achal Talati <achal.talati@oracle.com>

--- test/debug-project/test.js
+++
@@ -1,9 +0,0 @@
-const line = 'line';
-const num = 5;
-const bool = false;
-for (let i = 1; i < 10; i++) {
-    console.log(`${line}${i}`);
-}
-setTimeout(() => {
-    console.log('done');
-}, 2000);
--- test/test-project/src/test/system/clipboard.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-describe('System clipboard', function () {
-    it('Should contains same text as before tests were run', async function () {
-        const cb = await import('clipboardy');
-        const clipboard = cb.default.readSync();
-        expect(clipboard.startsWith('hello_ExTester'), `Fail, the clipboard is: '${clipboard}'`).to.be.true;
-    });
-});
